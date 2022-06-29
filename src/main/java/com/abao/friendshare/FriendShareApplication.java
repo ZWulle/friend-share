42d89f3dbf1ab8d1cae13737417d666c7aa2b296
xueshuang.bao <1013034379@qq.com>
2022-06-29 13:59:07 +0800

    first commit

---
+++ src/main/java/com/abao/friendshare/FriendShareApplication.java
@@ -0,0 +1,17 @@
+package com.abao.friendshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application启动类
+ *
+ * @author baoxueshuang
+ */
+@SpringBootApplication
+public class FriendShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendShareApplication.class, args);
+    }
+}
